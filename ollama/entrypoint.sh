e31a4a5119468499b0c77994dfbfb3014d98474b
Radi Aman Riyas <radiriyas@gmail.com>
2024-07-25 07:09:23 +0400

    feat: pull model on startup & mount volume for model storage

--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-# Pull the model
-ollama pull llama3
+# Start the Ollama service
+ollama serve &
 
-# Start the Ollama server
-ollama serve
+# Wait for the service to start
+sleep 2
+
+#Pull embedding model
+echo "Pulling embedding model...please wait"
+ollama pull mxbai-embed-large
+echo "Embedding model ready"
+
+# Pull the mistral model
+echo "Pulling mistral model......please wait"
+ollama pull mistral
+echo "Default mistral model ready"
+
+# Keep the container running
+tail -f /dev/null
